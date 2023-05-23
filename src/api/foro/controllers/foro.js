ee7aee5020c65f5567547c055416ca2be1588b6d
Juarrior <juanreig197@gmail.com>
2023-05-23 18:58:16 +0200

    Foros añadidos

---
+++ src/api/foro/controllers/foro.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foro controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::foro.foro');
---
+++ src/api/foro/routes/foro.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foro router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::foro.foro');
